61693bdff312bc59e65afc888e7c64419d285111
notscrappie <scr3ppie@gmail.com>
2022-06-01 14:33:50 +0300

    v4.2.1
    - Tweaked command handler to remove ghost commands (dev commands are now broken)

--- src/structures/handlers/commands.js
+++ src/structures/handlers/commands.js
@@ -1,15 +1,15 @@
 const glob = require('glob');
 const { Client } = require("discord.js")
-const { devGuildID } = require(`${process.cwd()}/src/structures/config.json`)
+const { devGuildID } = require("../config.json");
 const Ascii = require("ascii-table");
 
 module.exports = (client) => {
     /**
     * @param {Client} client
-    */
+     */
     const public_CommandsArray = [];
     const dev_CommandsArray = [];
-    
+
     const publicTable = new Ascii("Public Commands Loaded");
     const devTable = new Ascii("Developer Commands Loaded");
 
@@ -25,11 +25,9 @@ module.exports = (client) => {
             if(command.public) {
                 public_CommandsArray.push(command);
                 await publicTable.addRow(command.name, "🔷 Successfully loaded command.")
-                //console.log(blueBright(`Loaded ${command.name.toUpperCase()} from ${file.split("/").pop()}\n`))
             } else {
                 dev_CommandsArray.push(command);
                 await devTable.addRow(command.name, "🔷 Successfully loaded command.")
-                //console.log(`Loaded ${command.name.toUpperCase()} from ${file.split("/").pop()}`)
             }
             
             client.commands.set(command.name, command);
@@ -40,14 +38,10 @@ module.exports = (client) => {
         console.log(devTable.toString());
 
         client.on("ready", async () => {
-            client.publicCommands = public_CommandsArray;
-            const guild = client.guilds.cache.get(devGuildID);
-            if(guild) {
-                await guild.commands.set(dev_CommandsArray);
-                //console.log(`Loaded ${dev_CommandsArray.length} developer commands`);
-            }
-            await client.application.commands.set(public_CommandsArray);
-            //console.log(`Loading ${public_CommandsArray.length} global commands`);
-        });
+            client.guilds.cache.forEach((g) => {
+                if(g.id === devGuildID) g.commands.set(dev_CommandsArray);
+                else g.commands.set(public_CommandsArray);
+            })
+        })
     });
 }
